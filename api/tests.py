b5188569e5625636132c839be46999f3b0c7814a
kmazi <kingsleyu13@yahoo.com>
2019-05-05 15:41:30 +0100

    fix bug in flight factory class
     - update the flight model
    
     - update test to assert for fields from database query

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from factories import FlightFactory
+from .models import Flight
+
 
 # Create your tests here.
+class TestFlightModel(TestCase):
+    def test_flight_model_attr(self):
+        flight = FlightFactory()
+        flight_obj = Flight.objects.get(pk=flight.id)
+        self.assertIsNotNone(flight.name)
+        self.assertEqual(flight_obj.name, flight.name)
+        self.assertIsNotNone(flight.origin)
+        self.assertEqual(flight_obj.origin, flight.origin)
+        self.assertIsNotNone(flight.destination)
+        self.assertEqual(flight_obj.destination, flight.destination)
+        self.assertIsNotNone(flight.number)
+        self.assertEqual(flight_obj.number, flight.number)
+        self.assertIsNotNone(flight.departure_date)
+        self.assertEqual(flight_obj.departure_date, flight.departure_date)
+        self.assertIsNotNone(flight.departure_time)
+        self.assertEqual(flight_obj.departure_time, flight.departure_time)
--- user/models.py
+++ user/models.py
@@ -1,6 +1,10 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    email = models.EmailField(max_length=50, unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
