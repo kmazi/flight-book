6c7e2aceb0f5324a9429d3a3741e263fb9cf9663
kmazi <kingsleyu13@yahoo.com>
2019-06-18 20:45:12 +0100

    chore: Add flight model to admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
+"""Customize admin interface."""
+
 from django.contrib import admin
+from api.models import Flight
 
 # Register your models here.
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    """Flight admin customization."""
+    pass
