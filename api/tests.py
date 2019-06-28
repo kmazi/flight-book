2895eaca5c3077d8515145eaedd9f8e144a0a8f9
Touchstone <kingsleyu13@yahoo.com>
2019-06-28 15:31:59 +0100

    Enable users book flight (#11)
    * Enable users book flight
    
     - update tests
    
    * resolve coveralls error in circle ci
    
    * fix flight creation permission error
    
    * update coverage report

--- api/tests.py
+++ api/tests.py
@@ -1,11 +1,8 @@
 """Test flight functionalities."""
 # pylint: disable=no-member
 
+from datetime import datetime, timedelta
 
-from datetime import datetime as dt
-from datetime import timedelta
-
-import pytest
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -37,36 +34,54 @@ class TestFlightModel(TestCase):
         self.assertEqual(flight_obj.plane_type, flight.plane_type)
 
 
-class TestFlightReservation(APITestCase):
-    """Test flight reservation functionalities."""
+class BaseTestClass(APITestCase):
+    """Define base functionality for tests."""
 
     client = APIClient()
     User = get_user_model()
     data = {
         "origin": "lagos",
         "destination": "portharcourt",
-        "departure_date": dt.now().date() + timedelta(2),
-        "return_date": dt.now().date() + timedelta(7),
+        "departure_date": datetime.now().date() + timedelta(2),
+        "return_date": datetime.now().date() + timedelta(7),
         "plane_type": "business"
     }
 
     def setUp(self):
         """Define variables available to all test methods."""
         self.flights = [FlightFactory(), FlightFactory(), FlightFactory()]
-        self.test_date = dt.today().date() + timedelta(weeks=2)
+        self.test_date = datetime.today().date() + timedelta(weeks=2)
         self.user = self.User.objects.create(username="james",
                                              password="testing",
                                              email="james@gmail.com")
         self.client.force_authenticate(user=self.user)
 
-    def test_fail_to_book_flight_when_unauthenticated(self):
+
+class TestFlightCreation(BaseTestClass):
+    """Test flight creation functionalities."""
+
+    def setUp(self):
+        """Define custom setup for flight creation."""
+        super().setUp()
+        self.user.is_staff = True
+        self.user.save()
+
+    def test_fail_to_create_flight_when_unauthenticated(self):
         """User shouldn't be able to book flight when not authenticated."""
         self.client.force_authenticate(user=None)
         response = self.client.post(reverse("flight-list"), self.data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    @pytest.mark.one
-    def test_reserving_flight(self):
+    def test_only_admin_can_create_flights(self):
+        """App should disallow non admins from creating flights."""
+        self.user.is_staff = False
+        self.user.save()
+        response = self.client.post(reverse("flight-list"), self.data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(str(response.data["detail"]),
+                         "You do not have permission to perform this action.")
+
+    def test_creating_flight_as_admin(self):
         """Test that user can reserve flight."""
         response = self.client.post(reverse("flight-list"), self.data)
         booked_flight = Flight.records.get(
@@ -75,6 +90,7 @@ class TestFlightReservation(APITestCase):
         self.assertIsNotNone(response.data["id"])
         self.assertEqual(response.data["id"], booked_flight.id)
         self.assertEqual(response.data["origin"], self.data["origin"])
+        self.assertEqual(response.data["plane_type"], self.data["plane_type"])
         self.assertEqual(response.data["destination"],
                          self.data["destination"])
         self.assertEqual(response.data["departure_date"],
@@ -82,7 +98,11 @@ class TestFlightReservation(APITestCase):
         self.assertEqual(response.data["return_date"],
                          self.data["return_date"].strftime("%Y-%m-%d"))
 
-    def test_retrieving_reserved_flights(self):
+
+class TestFetchingFlight(BaseTestClass):
+    """Test fetching flight functionalities."""
+
+    def test_retrieving_flights(self):
         """Test that user can retrieve flight reservations."""
         flight_a = self.flights[0]
 
@@ -92,11 +112,12 @@ class TestFlightReservation(APITestCase):
         self.assertEqual(flight_res.departure_date, flight_a.departure_date)
         self.assertEqual(flight_res.return_date, flight_a.return_date)
         self.assertEqual(flight_res.origin, flight_a.origin)
+        self.assertEqual(flight_res.plane_type, flight_a.plane_type)
         self.assertEqual(flight_res.destination, flight_a.destination)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), len(self.flights))
 
-    def test_retrieving_reserved_flights_for_given_date(self):
+    def test_retrieving_flights_for_given_date(self):
         """Test that flights for a specific date can be retrieved."""
         FlightFactory(departure_date=self.test_date)
         response = self.client.get(
@@ -106,7 +127,7 @@ class TestFlightReservation(APITestCase):
         self.assertEqual(response.data[0]["departure_date"],
                          self.test_date.isoformat())
 
-    def test_retrieving_reserved_flights_by_return_dates(self):
+    def test_retrieving_flights_by_return_dates(self):
         """Test retrieving return flights scheduled for a specific days."""
         FlightFactory(return_date=self.test_date)
         response = self.client.get(
@@ -125,3 +146,42 @@ class TestFlightReservation(APITestCase):
                 base_url, self.test_date, self.flights[0].departure_date))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 0)
+
+
+class TestFlightBooking(BaseTestClass):
+    """Test functionality when booking flights."""
+
+    def test_book_flight_successfully(self):
+        """Users can book available flights with right credentials."""
+        test_flight = Flight.records.first()
+        response = self.client.patch(reverse('flight-book-flight',
+                                             kwargs={"pk": test_flight.id}),
+                                     data={"username": self.user.username})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["id"], test_flight.id)
+        self.assertEqual(response.data["origin"], test_flight.origin)
+        self.assertEqual(response.data["destination"], test_flight.destination)
+        self.assertEqual(response.data["departure_date"],
+                         test_flight.departure_date.isoformat())
+        self.assertEqual(response.data["plane_type"], test_flight.plane_type)
+        self.assertEqual(len(test_flight.passengers.all()), 1)
+
+    def test_fail_to_book_flight_for_unauthenticated_user(self):
+        """Unauthenticated user shouldn't book flight successfully."""
+        test_flight = Flight.records.first()
+        self.client.force_authenticate(user=None)
+        response = self.client.patch(reverse('flight-book-flight',
+                                             kwargs={"pk": test_flight.id}),
+                                     data={"username": self.user.username})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_fail_to_book_same_flight_more_than_once(self):
+        """App should fail to book same flight for a user more than once."""
+        test_flight = Flight.records.first()
+        test_flight.passengers.add(self.user)
+        response = self.client.patch(reverse('flight-book-flight',
+                                             kwargs={"pk": test_flight.id}),
+                                     data={"username": self.user.username})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(str(response.data[0]),
+                         "Flight has already been booked by you")
--- api/views.py
+++ api/views.py
@@ -1,27 +1,54 @@
 """Define view classes."""
 
-from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import api_view, permission_classes
+from django.contrib.auth import get_user_model
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
+from rest_framework.viewsets import ModelViewSet
 
 from .filters import FlightFilter
 from .models import Flight
+from .permissions import AllowAuthenicUserPatch, IsAdminWriteOnly
 from .serializers import FlightSerializer
 
 
 # Create your views here.
 class FlightViewset(ModelViewSet):
     """Flight viewset."""
+
     queryset = Flight.records.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          IsAdminWriteOnly | AllowAuthenicUserPatch)
     serializer_class = FlightSerializer
     filterset_class = FlightFilter
+
+    @action(detail=True, methods=["patch"])
+    def book_flight(self, request, pk=None):
+        """Handle patch requests."""
+        flight = self.get_object()
+        User = get_user_model()
+        user_name = request.data["username"]
+        try:
+            user = User.objects.get(username=user_name)
+        except Exception:
+            return Response(
+                data={"error": "An invalid user is trying to book a flight"},
+                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if flight.passengers.filter(username=user_name).exists():
+                raise ValidationError(
+                    detail="Flight has already been booked by you")
+            flight.passengers.add(user)
+            serializer = self.get_serializer(flight)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
 
 
 @csrf_exempt
 @api_view(["GET"])
-@permission_classes((AllowAny,))
+@permission_classes((AllowAny, ))
 def home(request):
     """Handle default request."""
     return Response(data="Welcome to flightbookie..")
