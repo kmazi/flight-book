f619cb0136de7ff90d5a15695046b5d8ac72905f
Touchstone <kingsleyu13@yahoo.com>
2019-06-29 00:18:42 +0100

    Implement email notification-#166465470 (#12)
    * feat: Implement email notification
    
     - update tests
     - update flight model
    
    * implement email push notification
    
    * Add test for mail notification
    
    * password validation

--- api/filters.py
+++ api/filters.py
@@ -5,8 +5,7 @@ from .models import Flight
 
 class FlightFilter(filters.FilterSet):
     departure = filters.DateFilter(field_name="departure_date")
-    return_date = filters.DateFilter(field_name="return_date")
 
     class Meta:
         model = Flight
-        fields = ["departure_date", "return_date"]
+        fields = ["departure_date"]
